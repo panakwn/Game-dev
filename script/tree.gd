dd39ef1c9936c95a8afa71b0209abfe424f0b5d0
panakwn <120510742+panakwn@users.noreply.github.com>
2025-02-18 12:31:18 +0200

    o

--- script/tree.gd
+++ script/tree.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var world_interaction: CollisionShape2D = $world_interaction
+@onready var character_body_2d: CharacterBody2D = $"../CharacterBody2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
