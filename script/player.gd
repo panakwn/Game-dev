e880b86413e9804c4722c6864c9395dffb83a0ac
Ropoperperithras <it2023071@hua.gr>
2025-02-20 12:47:45 +0200

    test

--- script/player.gd
+++ script/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var world_interaction: CollisionShape2D = $world_interaction
 @onready var collision_shape: CollisionShape2D = $CollisionShape
 
+var sliding_timer = 0.0
+var is_sliding = false
 const SPEED = 300.0
 const SPRINT_SPEED = 750.0
 const SLIDE_SPEED = SPRINT_SPEED * 10.0
@@ -20,9 +22,16 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 
+<<<<<<< Updated upstream
 	var input_x = Input.get_axis("left", "right")
 	var input_y = Input.get_axis("up", "down")
 	var move_dir = Vector2(input_x, input_y)
+=======
+	var directionx := Input.get_axis("left", "right")
+	var directiony := Input.get_axis("up", "down")
+	var sprint := Input.is_action_pressed("sprint")
+	var slide := Input.is_action_just_pressed("slide")
+>>>>>>> Stashed changes
 	
 	var is_sprinting := Input.is_action_pressed("sprint")
 	var is_sliding := Input.is_action_pressed("slide")
@@ -32,15 +41,33 @@ func _physics_process(delta: float) -> void:
 		speed = SPRINT_SPEED
 		animated_sprite.speed_scale = 2
 	else:
+<<<<<<< Updated upstream
 		animated_sprite.speed_scale = 1
+=======
+		current_speed = SPEED
+		animated_sprite.speed_scale  = 1
+	if slide and sprint: # animation gia to slide
+			animated_sprite.play("slide")
+			current_speed = SLIDE_SPEED
+			# tha to afjanei gia liga ms to speed kai meta tha to mionei stadiaka kai proodeutika
+			#while Input.action_press("slide"):
+				#tha mionei to speed nexri na stamatisei teleios (speed = 0) oso krataei patimeno to space
+			# otan to afinei to space apistrefei sto run speed
+>>>>>>> Stashed changes
 	
 	if move_dir != Vector2.ZERO:
 		move_dir = move_dir.normalized()
 		
+<<<<<<< Updated upstream
 		# elenxos gia slide otan kanei sprint
 		if is_sliding and is_sprinting:
 			if animated_sprite.animation != "slide":
 				animated_sprite.animation = "slide"
+=======
+	if velocity.x != 0 or velocity.y != 0:
+	
+		animated_sprite.play("run")
+>>>>>>> Stashed changes
 				
 				animated_sprite.play()
 				speed = SLIDE_SPEED
